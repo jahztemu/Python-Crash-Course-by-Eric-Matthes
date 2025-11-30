5018e296e542c0892781a9e7e4a870a781d0f357
Walker <jahtemu@gmail.com>
2025-11-30 21:03:21 +0100

    update

--- Chapter 9(Classes)/9.10.py
+++
@@ -1,4 +0,0 @@
-import modules.car as car
-
-my_tesla = car.ElectricCar("tesla", "model s", 2025)
-print(my_tesla.get_descriptive_name())
---
+++ Test/class import.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import Chapter_9_Classes.modules.car as car
+
+my_tesla = car.ElectricCar("tesla", "model s", 2025)
+print(my_tesla.get_descriptive_name())
+my_tesla.read_odometer()
+my_tesla.update_odometer(200)
+my_tesla.read_odometer()
+my_tesla.increment_odometer(100)
+my_tesla.read_odometer()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.get_range()
