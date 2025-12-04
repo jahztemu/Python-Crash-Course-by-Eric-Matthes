bbc378f324bdb12f81c1adfeb9f899f757ee1867
Walker <jahtemu@gmail.com>
2025-12-04 20:36:57 +0100

    update

--- chapter_10/10.1.py
+++ chapter_10/10.1.py
@@ -1,1 +1,7 @@
-ff
+with open("chapter_10\\module_text\\pi_digits.txt") as file_object:
+    contents = file_object.read()
+print(contents)
+print(contents.rstrip())
+
+for line in contents:
+    print(line)
